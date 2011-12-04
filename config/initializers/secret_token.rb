4fa111a88639f72f68676121fd1d23cdc8928054
Martin Cortez <martco@gmail.com>
2011-12-04 08:27:53 -0600

    Initial commit (using postgres)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quirkweek::Application.config.secret_token = 'baee6057ca27a595de8a44497bd63842618d661d45c51757a8a1dc5b629af3257116583f2111d23ad0f090af2d8f09a0dd94393bc723f6884dc35d8d18832468'
