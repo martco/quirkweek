0f30ec53dad5b2c2b93fda4ca2bc91a5edf07bf7
bsutic <bruno.sutic@gmail.com>
2011-12-14 18:45:28 +0100

    Added facebook gem, going to test it on heroku page

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "awGB84jnh21ImkYT8AwyA", "J46xK6VNhHxnL6IsqaKe1mj17cQHwEcq1VSscNLo8" 
+  provider :twitter, "awGB84jnh21ImkYT8AwyA", "J46xK6VNhHxnL6IsqaKe1mj17cQHwEcq1VSscNLo8"
+  provider :facebook, "292038604173207", "370f86f81650303d8f02ba7781fcbd0b"
 end
