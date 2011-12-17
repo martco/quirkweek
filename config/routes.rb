78591ccca7b55b919c6c44a05dd8459da5157d4f
bsutic <bruno.sutic@gmail.com>
2011-12-17 18:26:58 +0100

    began working on account page

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Quirkweek::Application.routes.draw do
   get "signup"         => "users#new"
   post "signup"        => "users#create"
   get "choose_signup"  => "sessions#choose_signup"
-  get "account"        => "users#myaccount"
+  get "account"        => "users#account"
   
   match "/auth/:provider/callback" => "sessions#create"
   match "/auth/failure"            => "sessions#failure"
