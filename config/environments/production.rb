62d973973a02c9ad0bd73bf1b6ec0b8f68bc3d77
bsutic <bruno.sutic@gmail.com>
2012-01-22 21:07:37 +0100

    one line change for heroku cedar stack

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,9 @@ Quirkweek::Application.configure do
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-
+  
+  config.action_dispatch.x_sendfile_header = nil  # config for heroku cedar stack
+  
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
